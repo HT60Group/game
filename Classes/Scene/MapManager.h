1c0db4f1e4302fbb3ad0ddf4add7d6f47dca5de0
908115501 <34595053+908115501@users.noreply.github.com>
2018-06-09 17:59:10 +0800

    Map
    地图移动添加及坐标转换

---
+++ Classes/Scene/MapManager.h
@@ -0,0 +1,20 @@
+#pragma once
+#include<cocos2d.h>
+#include<MapScene.h>
+using namespace cocos2d;
+
+class MapManager;
+
+class MapManager:public Node
+{
+public:
+	CREATE_FUNC(MapManager);
+	virtual bool init();
+
+	void SetMouseController();
+	void ControllerUpdate(float dt);
+private:
+	Point _mapMoveSpeed = Point(0, 0);//初始速度为0，向量
+	bool _isClick = false;
+	Point _mousePosition = Point(0, 0);
+};
