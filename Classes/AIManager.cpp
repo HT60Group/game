995ecedc61b29cf3783ddc41d1b6cba9affc6fae
908115501 <34595053+908115501@users.noreply.github.com>
2018-06-22 21:24:45 +0800

    hpy
    dsf

---
+++ Classes/AIManager.cpp
@@ -0,0 +1,57 @@
+#include"AIManager.h"
+#include"cocos2d.h"
+using namespace cocos2d;
+
+bool AIManager::init()
+{
+	/*selection = LayerColor::create(Color4B(0, 128, 128, 100));*/
+	//selection->setContentSize(CCSizeMake(120, 80));
+	/*selection->setPosition(Point(500, 500));*/
+	/*this->addChild(selection);*/
+	this->selectionController();
+	return true;
+}
+void AIManager::selectionController()
+{
+	auto listener = EventListenerTouchOneByOne::create();
+	
+	listener->onTouchBegan = [&](Touch* touch, Event* event)
+	{
+		auto _visibleSize = Director::getInstance()->getVisibleSize();
+		mousePos_began = touch->getLocation();
+		if (mousePos_began.x < (_visibleSize.width * 5 / 6))
+		{
+			log("began=(%f,%f)", mousePos_began.x, mousePos_began.y);
+			selection = LayerColor::create(Color4B(0, 128, 128, 100));
+			selection->setContentSize(CCSizeMake(0, 0));
+			this->addChild(selection);
+		}
+		return true;
+	};
+	listener->onTouchMoved = [&](Touch* touch, Event* event)
+	{
+		mousePos_moved = touch->getLocation();
+		auto _visibleSize = Director::getInstance()->getVisibleSize();
+	
+		if (mousePos_began.x < (_visibleSize.width * 5 / 6))
+		{
+			selection->setContentSize(CCSize(mousePos_moved.x - mousePos_began.x, mousePos_moved.y - mousePos_began.y));
+			selection->setPosition(mousePos_began);
+		}
+		
+
+		log("moved=(%f,%f)", mousePos_moved.x, mousePos_moved.y);
+
+	};
+	listener->onTouchEnded = [&](Touch* touch, Event* event)
+	{
+		mousePos_ended = touch->getLocation();
+		auto _visibleSize = Director::getInstance()->getVisibleSize();
+		log("ended=(%f,%f)", mousePos_ended.x, mousePos_ended.y);
+		if (mousePos_began.x < (_visibleSize.width * 5 / 6))
+		{
+			selection->removeFromParent();
+		}
+	};
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener,this);
+}
---
+++ Classes/AIManager.h
@@ -0,0 +1,22 @@
+#pragma once
+#include"cocos2d.h"
+#include<vector>
+#include"Entity/Army.h"
+using namespace cocos2d;
+class AIManager :public cocos2d::Layer
+{
+public:
+	CREATE_FUNC(AIManager);
+	virtual bool init();
+	void selectionController();
+	
+	/*LayerColor* layer;*/
+private:
+	Point mousePos_began = cocos2d::Point(0,0);
+	Point mousePos_moved = cocos2d::Point(0, 0);
+	Point mousePos_ended = cocos2d::Point(0, 0);
+	LayerColor* selection;
+	static std::vector<Army*> army_selected;
+	
+	
+};
